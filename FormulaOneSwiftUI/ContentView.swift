ff236c3f653ce7487a1014de39d2afb30f53e2c7
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-20 22:29:49 -0400

    Initial Commit

---
+++ FormulaOneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormulaOneSwiftUI
+//
+//  Created by Oscar Cristaldo on 2022-07-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FormulaOneSwiftUI/FormulaOneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneSwiftUIApp.swift
+//  FormulaOneSwiftUI
+//
+//  Created by Oscar Cristaldo on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
